44841a84b7eff8788d687b67ee18c7d27d686157
Jaylikethebirb <ananyajgupta@gmail.com>
2024-11-29 00:21:20 +0530

    Update and rename factorialrecursion.c to factorial.c

--- factorial.c
+++ factorial.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
-unsigned long long int getfactorial(int n) {
+unsigned long long int getfacrec(int n) {
     unsigned long long int fac= (unsigned long long int)n;
     if (n==0) {
         return 1;
     }else if (n==1) {
         return  1;
     }else {
-        fac=fac*getfactorial(n-1) ;
+        fac=fac*getfacrec(n-1) ;
+    }
+    return fac;
+}
+unsigned long long int getfac(int n){
+    unsigned long long int fac=1;
+    while(n>0){
+        fac*=n;
+        n--;
     }
     return fac;
 }
@@ -14,6 +22,6 @@ int main() {
     int n;
     printf("Enter number\t ");
     scanf("%d", &n);
-    printf("Factorial is\t  %llu \n", getfactorial(n));
+    printf("Factorial is\t  %llu \n", getfac(n));
     return 0;
 }
